87e9fe92f4761373bccebdfcac0ef3735a31e022
Nathan Vasquez <nathanvasquez@Nathans-MBP.fios-router.home>
2021-03-08 22:08:27 -0800

    assignment complete

--- Lab06/nvasquez12.cpp
+++ Lab06/nvasquez12.cpp
@@ -5,7 +5,7 @@
 void insert(std::list<int> *table, int key, int m);
 void Delete(std::list<int> *table, int key, int m);
 void search(std::list<int> *table, int key, int m);
-void output(std::list<int> *table, int key, int m);
+void output(std::list<int> *table, int m);
 int hash(int key, int m);
 
 int main(int argc, char ** argv) {
@@ -26,52 +26,62 @@ int main(int argc, char ** argv) {
             delete [] table;
             return 0;
         }
-        std:: cin >> key;
-        if (operation == 'i')
+        if (operation == 'i') {
+            std:: cin >> key;
             insert(table, key, m);
-        else if(operation == 'd')
+        }
+            
+        else if(operation == 'd') {
+            std:: cin >> key;
             Delete(table, key, m);
-        else if(operation == 's')
+        }
+            
+        else if(operation == 's') {
+            std:: cin >> key;
             search(table, key, m);
+        }
         else if(operation == 'o')
-            output(table, key, m);
+            output(table, m);
     }
         delete [] table;
         return 0;
 }
 
-void output(std::list<int> *table, int key, int m) {
+void output(std::list<int> *table, int m) {
     for(int i = 0; i < m; i++) {
+        std::cout << i << ":";
         for(std::list<int>::iterator it = table[i].begin(); it != table[i].end(); ++it) {
-            std::cout << i << ":" << *it << "->";
+            std::cout << *it << "->";
         }
         std::cout << ";\n";
     }
 }
 
 void insert(std::list<int> *table, int key, int m) {
-    table[hash(key, m)].push_back(key);
+    table[hash(key, m)].push_front(key);
 }
 
 void search(std::list<int> *table, int key, int m) {
     int i = 0;
     for(std::list<int>::iterator it = table[hash(key,m)].begin(); it != table[hash(key,m)].end(); ++it) {
         if(*it == key) {
-            std::cout << key << "FOUND_AT" << hash(key,m) << "," << i << ";\n"; 
+            std::cout << key << ":FOUND_AT" << hash(key,m) << "," << i << ";\n";
             return;
         }
         i++;
     }
-    std::cout << key << "NOT_FOUND;\n";
+    std::cout << key << ":NOT_FOUND;\n";
 }
 
 void Delete(std::list<int> *table, int key, int m) {
-    int x = table[hash(key,m)].size();
-    table[hash(key,m)].remove(key);
-    if(table[hash(key,m)].size() != x)
-        std::cout << key << "DELETED;\n";
-    else
-        std::cout << key << "DELETE_FAILED;\n";
+    for(std::list<int>::iterator it = table[hash(key,m)].begin(); it != table[hash(key,m)].end(); ++it) {
+        if(*it == key) {
+            table[hash(key,m)].erase(it);
+            std::cout << key << ":DELETED;\n";
+            return;
+        }
+    }
+    std::cout << key << ":DELETE_FAILED;\n";
 }
 
 int hash(int key, int m) {
